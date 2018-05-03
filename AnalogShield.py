3818252391016fe0e9665e319a70f931d9369916
wecassidy <wec.notif.github@gmail.com>
2018-05-03 17:25:08 -0400

    Initial commit with Python interface, Arduino code, and serial spec.

---
+++ AnalogShield.py
@@ -0,0 +1,400 @@
+from __future__ import print_function, division
+
+import numpy as np # For calculating means and standard deviations
+import os.path # For saving ADC and DAC calibration
+import pickle # For reading and saving calibration to a file
+import RigolInstruments as RI # For interfacing with the multimeter when calibrating
+import serial # For communicating with the Arduino
+import time
+import warnings
+
+class AnalogShield(object):
+    def __init__(self, address, shield_id, calibration_location=None):
+        """
+        address: the serial port of the shield.
+
+        shield_id: a unique identifier for each analog shield. It
+        doesn't matter what it is, but it must be different for each
+        shield.
+
+        calibration_location: if provided, DAC and ADC calibration
+        will be loaded from and saved to this file.
+        """
+
+        # A unique identifier for each analog shield - located on the deice
+        self.SHIELD_ID = shield_id
+
+        self.device = serial.Serial(port=address, baudrate=2e6, timeout=0)
+        time.sleep(3) # Ensure the first bytes of serial communication aren't dropped
+
+        self.ramp = {
+            "on": [False] * 4,
+            "period": [100] * 4,
+            "amplitude": [5] * 4,
+            "offset": [0] * 4,
+            "phase": [0]*4,
+            "function": ["triangle"] * 4
+        }
+
+        # Error correction functions - do nothing by default
+        self.adc_correct = [None] * 4
+        self.dac_correct = [None] * 4
+
+        # If provided, load the calibration from a file
+        self.calibration_location = calibration_location
+        if self.calibration_location is not None and os.path.exists(calibration_location):
+            with open(self.calibration_location) as calibration_file:
+                calibration = pickle.load(calibration_file)
+
+                if self.SHIELD_ID in calibration: # Check that this analog shield has a saved calibration
+                    self.adc_correct = calibration[self.SHIELD_ID]["adc"]
+                    self.dac_correct = calibration[self.SHIELD_ID]["dac"]
+
+        # Reset to known default state
+        self.queue_off()
+        self.ramp_off("all")
+        self.ramp_period("all", 100)
+        self.ramp_amplitude("all", 5)
+        self.ramp_offset("all", 0)
+        self.ramp_phase("all", 0)
+        self.ramp_function("all", "triangle")
+
+        self.analog_write("all", 0)
+
+        # For some reason, the first three readings of the ADC
+        # channels are sometimes 0x0000, 0x0000, 0x00**. After that,
+        # reading acts normally. This is a temporary fix to swallow
+        # those three strange values until a better solution is found.
+        for channel in range(4):
+            self.analog_read(channel, 5)
+
+    def write(self, identifier, arg=0):
+        """
+        Write a command to the Analog Shield and return the response.
+
+        The identifier is a two-character string (case-insensitive).
+        The argument to the command must be an 16-bit unsigned integer
+        (i.e. 0 <= arg <= 0xffff).  If the argument is omitted, two
+        null bytes will be sent.
+        """
+
+        ## Write the command
+        command = identifier + AnalogShield.encode_num(arg) # Build the command
+        self.device.write(command)
+
+        ## Read the response
+        # The response to a command is always terminated by a
+        # semicolon, so keep polling the input buffer until we read
+        # one.
+        response = self.device.read()
+        while response == "" or response[-1] != ";":
+            response += self.device.read()
+
+        # If the command was to read ADC values, it may have stopped
+        # reading the response early because one of the bytes was 0x3b
+        # (semicolon), the response terminator. This code makes sure
+        # all the response bytes (2*number of readings + terminator
+        # character) are read.
+        if identifier[0].lower() == "a" and response != "??;":
+            while len(response) < 2*arg+1:
+                response += self.device.read()
+
+        return response[:-1] # Strip the semicolon
+
+    def adc_calibrate(self, channel, multimeter_address):
+        """
+        There exists a scaling factor on each ADC channel, for some
+        reason. This method determines it and saves the results to a
+        compensation table (AnalogShield.adc_correct). To perform the
+        calibration, a Rigol DM3058 (or compatible) multimeter must be
+        attached to the computer.
+        """
+
+        # Connect to the multimeter
+        multimeter = RI.DM3058(multimeter_address)
+
+        actual_readings = []
+        adc_readings = []
+
+        # A big jump in DAC output occurs going to -5V, so give the multimeter extra time to adjust
+        self.analog_write(0, -5)
+        time.sleep(2)
+        for v_out in range(-5, 6): # Go from -5 to 5V in 1V steps
+            self.analog_write(0, v_out)
+
+            # Collect data
+            time.sleep(0.01) # Delay to let the multimeter adjust
+            v_actual = multimeter.voltage()
+            v_adc = np.mean(self.analog_read(channel, 500, correct=False)) # Average 500 readings to reduce noise
+
+            # Save data
+            actual_readings.append(v_actual)
+            adc_readings.append(v_adc)
+
+        # Calculate a linear regression that fits the error data
+        self.adc_correct[channel] = np.poly1d(np.polyfit(adc_readings, actual_readings, 1))
+
+        # If a calibration file was given, save the updated calibration
+        if self.calibration_location is not None:
+            # Read the existing calibrations, if the file exists
+            if os.path.exists(self.calibration_location):
+                with open(self.calibration_location) as calibration_file:
+                    calibration = pickle.load(calibration_file)
+            else:
+                calibration = {self.SHIELD_ID: {"adc": self.adc_correct, "dac": self.dac_correct}}
+
+            # Update the calibration
+            calibration[self.SHIELD_ID]["adc"][channel] = self.dac_correct[channel]
+
+            with open(self.calibration_location, "w") as calibration_file:
+                pickle.dump(calibration, calibration_file)
+
+    def dac_calibrate(self, channel, multimeter_address):
+        # Connect to the multimeter
+        multimeter = RI.DM3058(multimeter_address)
+
+        input_v = [v for v in range(-5, 6)]
+        dac_output = []
+
+        # A big jump in DAC output occurs going to -5V, so give the multimeter extra time to adjust
+        self.analog_write(channel, -5, correct=False)
+        time.sleep(2)
+        for v_out in input_v: # Go from -5 to 5V in 1V steps
+            self.analog_write(0, v_out, correct=False)
+
+            # Collect data
+            time.sleep(0.01) # Delay to let the multimeter adjust
+            v_actual = multimeter.voltage()
+
+            # Save data
+            dac_output.append(v_actual)
+
+        # Calculate a linear regression that fits the error data
+        self.dac_correct[channel] = np.poly1d(np.polyfit(dac_output, input_v, 1))
+
+        # If a calibration file was given, save the updated calibration
+        if self.calibration_location is not None:
+            # Read the existing calibrations, if the file exists
+            if os.path.exists(self.calibration_location):
+                with open(self.calibration_location) as calibration_file:
+                    calibration = pickle.load(calibration_file)
+            else:
+                calibration = {self.SHIELD_ID: {"adc": self.adc_correct, "dac": self.dac_correct}}
+
+            # Update the calibration
+            calibration[self.SHIELD_ID]["dac"][channel] = self.dac_correct[channel]
+
+            with open(self.calibration_location, "w") as calibration_file:
+                pickle.dump(calibration, calibration_file)
+
+    # Ramp methods
+    def ramp_running(self, channel):
+        return self.ramp["on"][channel]
+
+    def ramp_on(self, channel):
+        if channel == "all":
+            for c in range(4):
+                self.ramp_on(c)
+            return
+
+        self.ramp["on"][channel] = True
+        self.write("rc", channel)
+        return self.write("r1")
+
+    def ramp_off(self, channel):
+        if channel == "all":
+            for c in range(4):
+                self.ramp_off(c)
+            return
+
+        self.ramp["on"][channel] = False
+        self.write("rc", channel)
+        return self.write("r0")
+
+    def ramp_period(self, channel, time=None):
+        """
+        Set the period of the triangle wave.  Without an argument,
+        return the current value.
+        """
+
+        if channel == "all":
+            for c in range(4):
+                self.ramp_period(c, time)
+            return
+
+        if time is None:
+            return self.ramp["period"][channel]
+        elif time > 0:
+            self.ramp["period"][channel] = time
+            self.write("rc", channel)
+            return self.write("rp", time)
+        else:
+            raise ValueError("Period must be positive.")
+
+    def ramp_amplitude(self, channel, amp=None):
+        """
+        Set the amplitude of the triangle wave (equal to
+        V_max-V_average).  Without an argument, return the current
+        value.
+        """
+
+        if channel == "all":
+            for c in range(4):
+                self.ramp_amplitude(c, amp)
+            return
+
+        if amp is None:
+            return self.ramp["amplitude"][channel]
+        elif 0 <= amp <= 5:
+            self.ramp["amplitude"][channel] = amp
+            amp_bits = AnalogShield.volts_to_bits(amp)
+
+            self.write("rc", channel)
+            return self.write("ra", amp_bits)
+        else:
+            raise ValueError("Amplitude must be between 0V and 5V.")
+
+    def ramp_offset(self, channel, offset=None):
+        """
+        Set the offset of the triangle wave (equal to V_average-0V).
+        Without an argument, return the current value.
+        """
+
+        if channel == "all":
+            for c in range(4):
+                self.ramp_offset(c, offset)
+            return
+
+        if offset is None:
+            return self.ramp["offset"][channel]
+        elif -5 <= offset <= 5:
+            self.ramp["offset"][channel] = offset
+            offset_bits = AnalogShield.volts_to_bits(offset)
+
+            self.write("rc", channel)
+            return self.write("ro", offset_bits)
+
+    def ramp_phase(self, channel, phase=None):
+        """
+        Set the phase shift of the wave, expressed as a percentage of
+        the period. Without an argument, return the current value.
+        """
+
+        if channel == "all":
+            for c in range(4):
+                self.ramp_phase(c, phase)
+            return
+
+        if phase is None:
+            return self.ramp["phase"][channel]
+        elif 0 <= phase <= 100:
+            self.ramp["phase"][channel] = phase
+            phase_bits = int(phase * 65535/100) # Convert from percent to bits
+
+            self.write("rc", channel)
+            return self.write("rs", phase_bits)
+
+    def ramp_function(self, channel, function=None):
+        """
+        Set the function used to generate the wave. Without an
+        argument, return the current value. Acceptable values:
+        triangle, sin, and square.
+        """
+
+        if channel == "all":
+            for c in range(4):
+                self.ramp_function(c, function)
+            return
+
+        if function is None:
+            return self.ramp["function"][channel]
+        elif function in ("triangle", "sin", "square"):
+            self.ramp["function"][channel] = function
+            func_num = {"triangle":0, "sin":1, "square":2}[function]
+
+            self.write("rc", channel)
+            return self.write("rf", func_num)
+        else:
+            raise ValueError("Invalid ramp function: {}".format(function))
+
+    # DAC methods
+    def analog_write(self, channel, val, correct=True):
+        """Set the value on one of the DACs."""
+
+        # Apply correction function, if desired
+        if correct and channel != "all":
+            if self.dac_correct[channel] is not None:
+                val = self.dac_correct[channel](val)
+
+                # Make sure the corrected value stays within range
+                val = max(-5, val)
+                val = min(5, val)
+            else:
+                warnings.warn("DAC channel {} is not yet calibrated.".format(channel), RuntimeWarning, stacklevel=2)
+
+        val_bits = AnalogShield.volts_to_bits(val)
+        if isinstance(channel, str) and channel.lower() == "all":
+            return self.write("va", val_bits)
+        elif 0 <= channel <= 3:
+            return self.write("v"+str(channel), val_bits)
+
+    # ADC methods
+    def analog_read(self, channel, samples=1, correct=True):
+        """
+        Read one or more values off of the ADC.
+
+        This method returns an array of voltages.  The samples are
+        taken as fast as possible, without a regular delay.
+        """
+
+        if 0 <= channel <= 3:
+            # Extract values from the response
+            response = self.write("A"+str(channel), samples)
+            bit_vals = []
+            for (msb, lsb) in zip(response[0::2], response[1::2]):
+                bit_vals.append((ord(msb) << 8) + ord(lsb))
+
+            # Convert to volts
+            voltages = [AnalogShield.bits_to_volts(b) for b in bit_vals]
+
+            # Apply correction function, if desired
+            if correct:
+                if self.adc_correct[channel] is not None:
+                    voltages = [self.adc_correct[channel](v) for v in voltages]
+                else:
+                    warnings.warn("ADC channel {} is not yet calibrated.".format(channel), RuntimeWarning, stacklevel=2)
+
+            return voltages
+        else:
+            raise ValueError("Invalid channel: {}".format(channel))
+
+    # Queue methods
+    def queue_on(self):
+        return self.write('qm', 1)
+
+    def queue_off(self):
+        return self.write('qm', 0)
+
+    @staticmethod
+    def bits_to_volts(bits):
+        """Convert a voltage as encoded by the Analog Shield into volts."""
+
+        return 2*bits/13107 - 5
+
+    @staticmethod
+    def volts_to_bits(volts):
+        """Convert volts into a 16-bit number for the Analog Shield."""
+
+        return int((13107*volts + 65535)/2)
+
+    @staticmethod
+    def encode_num(n):
+        """
+        Convert a 16-bit number to a two-character string where the 8 MSB
+        are converted to the first character and the 8 LSB are converted
+        to the second character.
+        """
+
+        return chr(n >> 8) + chr(n & 0x00ff)
+
+AS = AnalogShield
---
+++ analogue_serial.ino
@@ -0,0 +1,191 @@
+#include <analogShield.h>
+#include <SPI.h>
+
+const unsigned int ZERO_V = 32767;
+
+// Queue settings
+unsigned int queue = 0;
+int queue_pin = 7;
+
+// DAC values
+unsigned int dac[4];
+
+// Ramp default settings
+boolean ramp_enabled[4] = {false, false, false, false};
+int ramp_channel = 0;
+unsigned int ramp_period[4] = {100 * 1e3, 100 * 1e3, 100 * 1e3, 100 * 1e3}; // Units: microseconds
+unsigned int ramp_amplitude[4] = {ZERO_V, ZERO_V, ZERO_V, ZERO_V}; // Units: volts
+unsigned int ramp_offset[4] = {ZERO_V, ZERO_V, ZERO_V, ZERO_V}; // Units volts
+float ramp_phase[4] = {0, 0, 0, 0}; // Units: percent of period
+unsigned int ramp_function[4] = {0, 0, 0, 0};
+
+// Change ramp settings - returns 0 on success, -1 on error
+int ramp_settings(char function, unsigned short arg) {
+  if (function == '0') { // Ramp off
+    ramp_enabled[ramp_channel] = false;
+    analog.write(ramp_channel, ZERO_V); // Reset the channel
+  } else if (function == '1') { // Ramp on
+    ramp_enabled[ramp_channel] = true;
+  } else if (function == 'C' || function == 'c') {
+    if (arg <= 3) { // Check channel
+      ramp_channel = arg;
+    } else { // Invalid channel; return error status
+      return -1;
+    }
+  } else if (function == 'P' || function == 'p') { // Set period
+    ramp_period[ramp_channel] = arg * 1e3; // Convert milliseconds to microseconds
+  } else if (function == 'A' || function == 'a') { // Set amplitude
+    ramp_amplitude[ramp_channel] = arg-ZERO_V;
+  } else if (function == 'O' || function == 'o') { // Set offset
+    ramp_offset[ramp_channel] = arg;
+  } else if (function == 'S' || function == 's') { // Set phase
+    ramp_phase[ramp_channel] = arg * 1.0/0xffff; // Convert from bits to percentage
+  } else if (function == 'F' || function == 'f') { // Set function
+    ramp_function[ramp_channel] = arg;
+  } else { // Unrecognized function
+    return -1;
+  }
+
+  Serial.print("OK");
+  return 0; // Return success status
+}
+
+// Calculate the voltage output as a function of time (microseconds)
+// V(t) = amplitude * (|(t - phase shift) % period - period/2| / (period/4) - 1) + offset
+long  ramp_triangle(unsigned long t, int channel) {
+  return ramp_amplitude[channel] * (abs((t - (int)(ramp_phase[channel]*ramp_period[channel]))%ramp_period[channel] - ramp_period[channel]/2.0) / (ramp_period[channel]/4.0) - 1) + ramp_offset[channel];
+}
+
+// V(t) = amplitude * sin(2pi/period * (t - phase shift)) + offset
+long ramp_sin(unsigned long t, int channel) {
+  return ramp_amplitude[channel] * sin(TWO_PI/ramp_period[channel] * (t - ramp_phase[channel]*ramp_period[channel])) + ramp_offset[channel];
+}
+
+// V(t) = amplitude * (-1)^floor((t - phase shift) / period) + offset
+long ramp_square(unsigned long t, int channel) { // TODO doesn't work
+  return ramp_amplitude[channel] * pow(-1, (unsigned long)((t - ramp_phase[channel]*ramp_period[channel])/ramp_period[channel])) + ramp_offset[channel];
+}
+
+int set_dac(char channel, unsigned short val) {
+  if (channel == 'A' || channel == 'a') {
+    dac[0] = val;
+    dac[1] = val;
+    dac[2] = val;
+    dac[3] = val;
+
+    analog.write(val, val, val, val, true);
+  } else if ('0' <= channel && channel <= '3') {
+    dac[channel-'0'] = val;
+
+    analog.write(channel-'0', val);
+  } else {
+    return -1; // Invalid channel
+  }
+
+  Serial.print("OK");
+  return 0;
+}
+
+int get_adc(char channel, unsigned short arg) {
+  if ('0' <= channel && channel <= '3') {
+    for (int i = 0; i < arg; i++) {
+      unsigned short reading = analog.read(channel-'0');
+
+      Serial.write(reading >> 8); // Write the first byte
+      Serial.write(reading & 0x00ff); // Write the second byte
+    }
+  } else {
+    return -1; // Invalid channel
+  }
+
+  return 0;
+}
+
+int queue_settings(char function, unsigned short arg) {
+  if (function == 'M' || function == 'm') {
+    queue = arg;
+  } else {
+    return -1; // Invalid command
+  }
+
+  Serial.print("OK");
+  return 0;
+}
+
+void setup() {
+  SPI.setClockDivider(SPI_CLOCK_DIV2);
+  Serial.begin(2e6);
+
+  pinMode(queue_pin, INPUT);
+
+  // Set all channels to 0V
+  dac[0] = ZERO_V;
+  dac[1] = ZERO_V;
+  dac[2] = ZERO_V;
+  dac[3] = ZERO_V;
+  analog.write(ZERO_V, ZERO_V, ZERO_V, ZERO_V, true);
+}
+
+unsigned char cmd[4];
+int bytes_read = 0;
+void loop() {
+  // Read more of the command, if it's available
+  while (Serial.available() > 0 && bytes_read < 4) {
+    cmd[bytes_read] = Serial.read();
+    bytes_read++;
+  }
+
+  // If the full command has been received, process it
+  if (bytes_read == 4) {
+    // Wait for the queue pin to go HIGH in queue mode
+    if (queue != 0) {
+      while (digitalRead(queue_pin) != HIGH) {}
+    }
+
+    // Extract identifier and argument
+    char ident[] = {cmd[0], cmd[1]};
+    unsigned short arg = (cmd[2] << 8) + cmd[3];
+
+    int status = 0;
+    if (ident[0] == 'R' || ident[0] == 'r') {
+      status = ramp_settings(ident[1], arg);
+    } else if (ident[0] == 'V' || ident[0] == 'v') {
+      status = set_dac(ident[1], arg);
+    } else if (ident[0] == 'A' || ident[0] == 'a') {
+      status = get_adc(ident[1], arg);
+    } else if (ident[0] == 'Q' || ident[0] == 'q') {
+      status = queue_settings(ident[1], arg);
+    } else { // Unrecognized command
+      Serial.print("??");
+    }
+
+    if (status != 0) { // Something went wrong
+      Serial.print("??");
+    }
+
+    Serial.print(';'); // Always terminate with a semicolon
+    Serial.flush();
+    bytes_read = 0; // Ready to start reading another command
+  }
+
+  // Ramp
+  for (int i = 0; i < 4; i++) { // For each channel
+    if (ramp_enabled[i]) {
+      unsigned long t = micros();
+
+      long v; // Calculate new value
+      if (ramp_function[i] == 0) {
+        v = ramp_triangle(t, i);
+      } else if (ramp_function[i] == 1) {
+        v = ramp_sin(t, i);
+      } else if (ramp_function[i] == 2) {
+        v = ramp_square(t, i);
+      }
+
+      // Clip the ramp if it goes out of the voltage range
+      v = constrain(v, 0, 0xffff);
+
+      analog.write(i, v);
+    }
+  }
+}
